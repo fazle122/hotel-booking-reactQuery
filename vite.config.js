6b775e806fc4737fa43213c489c5e99911452f2c
fr <fazlerabby122@gmail.com>
2024-08-29 22:11:17 +0600

    vite confing update1

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,16 @@ import eslint from 'vite-plugin-eslint'
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{
-    proxy:{
-      // '/api':'http://localhost:8000'
-      '/api':'https://hotel-booking-backend-kohl.vercel.app'
+    // proxy:{
+    //   // '/api':'http://localhost:8000'
+    //   '/api':'https://hotel-booking-backend-kohl.vercel.app'
+    // }
+    proxy: {
+      '/api': {
+        target: 'https://hotel-booking-backend-kohl.vercel.app',
+        changeOrigin: true,
+        secure: false,
+      }
     }
   },
   plugins: [react(),eslint()],
