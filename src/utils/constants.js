2710396862fbf56407925d48c0771e75694f2ef9
fr <fazlerabby122@gmail.com>
2024-08-30 10:34:14 +0600

    base url update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,8 @@
 
 
 
-const BASE_URL_LOCAL = "https://hotel-booking-backend-kohl.vercel.app"
-const BASE_URL = "http://localhost:8000"
+const BASE_URL = "https://hotel-booking-backend-kohl.vercel.app"
+const BASE_URL_LOCAL = "http://localhost:8000"
 
 export {BASE_URL,BASE_URL_LOCAL}
 
